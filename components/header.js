086e9d84a524ccc8a410304b7391aefb48d8fde8
Henry Smith <henry.smith@netlify.com>
2023-06-29 12:36:54 +0100

    Update UI

---
+++ components/header.js
@@ -0,0 +1,67 @@
+import {
+  LifebuoyIcon,
+  NewspaperIcon,
+  PhoneIcon,
+} from "@heroicons/react/20/solid";
+
+const cards = [
+  {
+    name: "Sales",
+    description:
+      "Consectetur vel non. Rerum ut consequatur nobis unde. Enim est quo corrupti consequatur.",
+    icon: PhoneIcon,
+  },
+  {
+    name: "Technical Support",
+    description:
+      "Quod possimus sit modi rerum exercitationem quaerat atque tenetur ullam.",
+    icon: LifebuoyIcon,
+  },
+  {
+    name: "Media Inquiries",
+    description:
+      "Ratione et porro eligendi est sed ratione rerum itaque. Placeat accusantium impedit eum odit.",
+    icon: NewspaperIcon,
+  },
+];
+
+export default function Example() {
+  return (
+    <div className="relative isolate overflow-hidden bg-gray-900 py-40 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8 z-10 absolute top-30 left-20">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+            Find your perfect fit.
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-300">
+            Top quality trainers for any situation
+          </p>
+        </div>
+      </div>
+      <div className="bg-gray-800 absolute top-0 left-0 h-full w-full bg-opacity-80"></div>
+      <img
+        src="https://images.prismic.io/containerstoriesproduction/b5b0a0438c661fb01bc0ffa28c6676681e0981a9_shoe-boxes_black_1600x1067.jpg?auto=format"
+        alt=""
+        className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
+      />
+      <div className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl">
+        <div
+          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
+      </div>
+      <div className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu">
+        <div
+          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
+      </div>
+    </div>
+  );
+}
---
+++ netlify/functions/fetch-all-products.js
@@ -0,0 +1,9 @@
+import { fetchAllProducts } from "../../lib/api";
+
+exports.handler = async (event) => {
+  const products = await fetchAllProducts();
+  return {
+    statusCode: 200,
+    body: `<pre> ${JSON.stringify(products, "", 2)} </pre/`,
+  };
+};
