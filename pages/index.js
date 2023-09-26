b1e24dada42fe53c074c57c9a11ec84c28989f23
Henry Smith <henry.smith@netlify.com>
2023-09-26 14:21:32 +0100

    Update all of these

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import { fetchAllProducts } from "../lib/api";
+import { fetchPageData } from "../lib/apiContentful";
+
 import NavBar from "../components/navBar";
 import Header from "../components/header";
 
@@ -6,9 +7,10 @@ import Image from "next/image";
 import Link from "next/link";
 
 export const getStaticProps = async () => {
-  const products = await fetchAllProducts();
+  const products = await fetchPageData("/");
+
   return {
-    props: { products: products?.data?.allContentfulProduct?.nodes },
+    props: { products },
   };
 };
 
@@ -24,13 +26,13 @@ export default function Home({ products }) {
         <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
           {products.map((product) => (
             <div
-              key={product.id}
+              key={product.name}
               className="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white"
             >
               <div className="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96">
                 <Image
-                  src={product.images[0].image.url}
-                  alt={product.images[0].image.altImage}
+                  src={product?.imagesCollection?.items[0]?.image?.url}
+                  alt={product?.imagesCollection?.items[0]?.image?.description}
                   width={500}
                   height={500}
                   className="h-full w-full object-cover object-center sm:h-full sm:w-full"
@@ -38,16 +40,14 @@ export default function Home({ products }) {
               </div>
               <div className="flex flex-1 flex-col space-y-2 p-4">
                 <h3 className="text-sm font-medium text-gray-900">
-                  <Link href={product.href || "/"}>
+                  <Link href={product?.href || "/"}>
                     <span aria-hidden="true" className="absolute inset-0" />
                     {product.name}
                   </Link>
                 </h3>
                 <p className="text-sm text-gray-500">{product.description}</p>
                 <div className="flex flex-1 flex-col justify-end">
-                  <p className="text-base font-medium text-gray-900">
-                    £{product.price}
-                  </p>
+                  <p className="text-base font-medium text-gray-900"></p>
                 </div>
               </div>
             </div>
