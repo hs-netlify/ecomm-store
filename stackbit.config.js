2fbb14589d79622d1c1fa3cb72f73645250778f1
Henry Smith <henry.smith@netlify.com>
2023-09-26 14:28:04 +0100

    Remove node

--- stackbit.config.js
+++ stackbit.config.js
@@ -3,6 +3,7 @@ import { defineStackbitConfig } from "@stackbit/types";
 
 export default defineStackbitConfig({
   stackbitVersion: "~0.6.0",
+  nodeVersion: 12,
   contentSources: [
     new ContentfulContentSource({
       spaceId: process.env.CONTENTFUL_SPACE_ID,
