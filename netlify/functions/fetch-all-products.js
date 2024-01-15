5b1bd094c58f805621ff93ce4ee9bd5065338b14
Henry Smith <henry.smith@netlify.com>
2024-01-15 09:00:27 +0000

    Remove functions

--- netlify/functions/fetch-all-products.js
+++
@@ -1,9 +0,0 @@
-import { fetchAllProducts } from "../../lib/api";
-
-exports.handler = async (event) => {
-  const products = await fetchAllProducts();
-  return {
-    statusCode: 200,
-    body: `<pre> ${JSON.stringify(products, "", 2)} </pre>`,
-  };
-};
