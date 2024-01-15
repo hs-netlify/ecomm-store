2606a432a07f429558984f0738ba97f108cc37f9
Henry Smith <henry.smith@netlify.com>
2024-01-15 15:31:45 +0000

    Add form data

--- components/form.js
+++ components/form.js
@@ -92,8 +92,15 @@ const inputHandler = (input, i) => {
 const CustomForm = ({ form }) => {
   return (
     <div className="w-full flex flex-col items-center justify-center py-10">
-      <div className="h-40 w-full flex items-center p-12 text-5xl bg-gradient-to-br bg-black from-indigo-500 shadow">
-        {form?.label}
+      <div className="h-40 w-full bg-black flex items-center p-20 relative overflow-hidden shadow">
+        <img
+          src="images/trainers.png"
+          className="w-full h-40 object-cover opacity-40 absolute top-0 left-0 "
+        />
+
+        <h2 className="z-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">
+          {form?.label}
+        </h2>
       </div>
       <form
         data-sb-object-id={form?.id}
